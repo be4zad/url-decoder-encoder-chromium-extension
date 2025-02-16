ea8146c148455b199d8f7c0b5f5142ff39c175b5
Behzad <be4zad@tuta.io>
2025-02-16 11:29:41 +0330

    Add option to replace # with %23

--- popup.js
+++ popup.js
@@ -1,14 +1,42 @@
 (async () => {
-  const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-  document.getElementById('url-input').value = decodeURI(tab.url);
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
+
+  var url = decodeURI(tab.url);
+  url = url.replace("%23", "#");
+  document.getElementById("url-input").value = url;
+
+  chrome.storage.sync.get("encodeHashtagLastTimeChecked", function (data) {
+    if (data.encodeHashtagLastTimeChecked == true) {
+      document.getElementById("encode-hashtag").checked = true;
+    }
+  });
 })();
 
-document.getElementById('go-button').addEventListener('click', () => {
-  chrome.tabs.update(undefined, { url: document.getElementById('url-input').value });
+document.getElementById("go-button").addEventListener("click", () => {
+  var url = document.getElementById("url-input").value;
+
+  if (document.getElementById("encode-hashtag").checked) {
+    url = url.replace("#", "%23");
+  }
+
+  chrome.tabs.update(undefined, { url: url });
 });
 
-document.getElementById('url-input').addEventListener('keypress', (event) => {
+document.getElementById("url-input").addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
-    document.getElementById('go-button').click();
+    document.getElementById("go-button").click();
   }
 });
+
+document
+  .getElementById("encode-hashtag")
+  .addEventListener("change", function () {
+    if (this.checked) {
+      chrome.storage.sync.set({ encodeHashtagLastTimeChecked: true });
+    } else {
+      chrome.storage.sync.set({ encodeHashtagLastTimeChecked: false });
+    }
+  });
